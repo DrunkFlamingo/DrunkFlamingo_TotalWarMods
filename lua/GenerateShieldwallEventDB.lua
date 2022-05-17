6d548439bf3b24e77b79d8e2680aed1f2ba5c6af
DrunkFlamingo <matte901@gmail.com>
2022-05-17 13:24:54 -0400

    IC - new event gen script

---
+++ lua/GenerateShieldwallEventDB.lua
@@ -0,0 +1,21 @@
+local region_sets = {
+    ["MAJOR_SETTLEMENTS"] = "",
+    ["MINOR_SETTLEMENTS"] = "",
+    ["ALL_SETTLEMENTS"] = "",
+    ["FOREIGNERS"] = "",
+    ["BOROUGHS"] = "",
+    ["LONGPHORTS"] = "",
+    ["MARKETS"] = "",
+    ["MONASTERIES"] = ""
+}
+
+local faction_sets = {
+    ["ANY_FACTION"] = "",
+    ["ENG_FACTIONS"] = "",
+    ["DAN_FACTIONS"] = "",
+    ["VSK_FACTIONS"] = "",
+    ["IRI_FACTIONS"] = "",
+    ["SCO_FACTIONS"] = "",
+    ["WEL_FACTIONS"] = "",
+    ["VIK_FACTIONS"] = "",
+}
